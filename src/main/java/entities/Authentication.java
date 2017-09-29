5edfe7de14896894a9539deca6e684ee5c03010b
Joaquin Zamora <joaquin_z619@hotmail.com>
2017-09-29 17:48:24 +0200

    Creadas las entidades básicas de ejemplo

---
+++ src/main/java/entities/Authentication.java
@@ -0,0 +1,15 @@
+package entities;
+
+public class Authentication {
+    
+    private int id;
+    
+    public Authentication(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+}
