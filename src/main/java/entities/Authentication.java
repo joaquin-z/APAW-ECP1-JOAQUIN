3ee7089239a97fbd8d2b5aa4de81917c5511beb6
Joaquin Zamora <joaquin_z619@hotmail.com>
2017-09-30 23:43:23 +0200

    Cambio en clase Authentication original

--- src/main/java/entities/Authentication.java
+++ src/main/java/entities/Authentication.java
@@ -1,15 +1,46 @@
 package entities;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Authentication {
     
     private int id;
     
-    public Authentication(int id) {
+    private Map<Integer, User> users;
+    
+    private Map<Integer, Rol> rols;
+    
+    public Authentication() {
+        this.id = 0;
+        this.users = new HashMap<>();
+        this.rols = new HashMap<>();
+    }
+    
+    public void setAuthentication(int id, Rol rol, User user) {
         this.id = id;
+        this.rols.put(id,rol);
+        this.users.put(id,user);
     }
 
     public int getId() {
         return id;
+    }
+
+    public User getUser(int key) {
+        return users.get(key);
+    }
+
+    public void removeUser(int key) {
+        this.users.remove(key);
+    }
+    
+    public Rol getRol(int key) {
+        return rols.get(key);
+    }
+
+    public void removeRol(int key) {
+        this.rols.remove(key);
     }
 
 }
